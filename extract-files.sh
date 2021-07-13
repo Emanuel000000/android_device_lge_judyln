b398370860aad774cd1081698bd5b40c03cf0b6b
Emanuel <emanuelghub@gmail.com>
2021-07-13 19:44:04 +0400

    judyln: Swich to standalone tools

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 The LineageOS Project
-# Copyright (C) 2019-2020 Paranoid Android
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 The PixelExperience Project
+# Copyright (C) 2020 The Evolution X Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,42 +25,45 @@ VENDOR=lge
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="$ROOT"/vendor/dot/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
+SECTION=
+KANG=
 
 while [ "$1" != "" ]; do
-    case $1 in
+    case "$1" in
         -n | --no-cleanup )     CLEAN_VENDOR=false
-        ;;
+                                ;;
+        -k | --kang)            KANG="--kang"
+                                ;;
         -s | --section )        shift
-        SECTION=$1
-        CLEAN_VENDOR=false
-        ;;
-        * )                     SRC=$1
-        ;;
+                                SECTION="$1"
+                                CLEAN_VENDOR=false
+                                ;;
+        * )                     SRC="$1"
+                                ;;
     esac
     shift
 done
 
-if [ -z "$SRC" ]; then
-    SRC=/home/darren/stock/30f/system/
+if [ -z "${SRC}" ]; then
+    SRC=/home/darren/stock/30f/system
 fi
 
-# Initialize the helper for common device
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-setup_vendor "$DEVICE" "$VENDOR" "$ROOT" false "$CLEAN_VENDOR"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
-
-"$MY_DIR"/setup-makefiles.sh
+"${MY_DIR}/setup-makefiles.sh"
